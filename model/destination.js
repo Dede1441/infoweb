539c5bea9f80bd874c7dc49b2f45b5a6225b770a
Dede1441 <romain.guernier@gmail.com>
2021-04-15 22:07:37 +0200

    update alpha
    prix NOK
    commentaire NOK
    base de donné OK

--- model/destination.js
+++ model/destination.js
@@ -20,13 +20,34 @@ class News {
     try {
       this.news = await connexion.query('SELECT country, destination, photo, description FROM voyages ORDER BY country')
     } catch (anError) {
-      console.log('Error to get news !')
+      console.log('Error to get destinations !')
 
       // See error from SQL news
       // console.log(anError);
     }
   }
 
+  async setCommentary () {
+    try {
+      this.news = await connexion.query('INSERT INTO "main"."commentaire" ("id","commentaire","date") VALUES (id,input,date);')
+    } catch (anError) {
+      console.log('Error to set commentary !')
+
+      // See error from SQL news
+      // console.log(anError);
+    }
+  }
+  async getcommentaireList () {
+    try {
+      this.news = await connexion.query('SELECT commentaire FROM commentaire ORDER BY date')
+    } catch (anError) {
+      console.log('Error to get commentaire !')
+
+      // See error from SQL news
+      // console.log(anError);
+    }
+  }
+  
   getNews () {
     return this.news
   }
